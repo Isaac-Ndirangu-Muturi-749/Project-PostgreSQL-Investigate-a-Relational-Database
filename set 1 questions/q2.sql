cb01d408a20147b0d0b9428aadbe11c579ba5fb8
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-08-24 22:11:30 +0300

    Create q2.sql

---
+++ set 1 questions/q2.sql
@@ -0,0 +1,62 @@
+/*  Question 2
+Now we need to know how the length of rental duration of these family-friendly
+ movies compares to the duration that all movies are rented for. Can you
+  provide a table with the movie titles and divide them into
+   4 levels (first_quarter, second_quarter, third_quarter, and final_quarter) 
+   based on the quartiles (25%, 50%, 75%) of the average rental 
+   duration(in the number of days) for movies across all categories? 
+   Make sure to also indicate the category that these family-friendly movies 
+   fall into.*/
+
+
+
+-- Create a Common Table Expression (CTE) to select family movie rental durations and their categories
+WITH FamilyRentalDurations AS (
+    SELECT
+        category.name AS "Category",
+        film.title AS "Film title",
+        film.rental_duration AS "Rental Duration"
+    FROM
+        film
+    JOIN
+        film_category ON film.film_id = film_category.film_id
+    JOIN
+        category ON film_category.category_id = category.category_id
+    WHERE
+        category.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+),
+
+-- Create another CTE to calculate percentiles for rental durations within each category
+CategoryAverages AS (
+    SELECT
+        category.name AS "Category",
+        PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY film.rental_duration) AS "25th Percentile",
+        PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY film.rental_duration) AS "50th Percentile",
+        PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY film.rental_duration) AS "75th Percentile"
+    FROM
+        film
+    JOIN
+        film_category ON film.film_id = film_category.film_id
+    JOIN
+        category ON film_category.category_id = category.category_id
+    GROUP BY
+        category.name
+)
+
+-- Combine the two CTEs to generate the final table with movie titles categorized by quartiles
+SELECT
+    frd."Film title",
+    frd."Category",
+    frd."Rental Duration",
+    CASE
+        WHEN frd."Rental Duration" <= ca."25th Percentile" THEN '1'
+        WHEN frd."Rental Duration" <= ca."50th Percentile" THEN '2'
+        WHEN frd."Rental Duration" <= ca."75th Percentile" THEN '3'
+        ELSE '4'
+    END AS "Rental Duration quartile"
+FROM
+    FamilyRentalDurations frd
+JOIN
+    CategoryAverages ca ON frd."Category" = ca."Category"
+ORDER BY
+    frd."Rental Duration" ASC; -- Sort the results by duration in ascending order
